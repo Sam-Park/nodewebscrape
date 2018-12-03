66d70e82ac2441d1263bf8054ec0c268f742f60e
unknown <sam.park.email@gmail.com>
2018-12-03 16:14:35 -0500

    scraping hrefs from a tags correctly

--- potusScraper.js
+++ potusScraper.js
@@ -1,10 +1,16 @@
 const rp = require('request-promise');
+const cheerio = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 rp(url)
     .then(function(html){
         //success!
-        console.log(html);
+        console.log(cheerio('big > a', html).length);
+        const wikiUrls = [];
+        for (let i = 0; i < 45; i++) {
+            wikiUrls.push(cheerio('big > a', html)[i].attribs.href)
+        }
+        console.log(wikiUrls);
     })
     .catch(function(err){
         console.log("Error fetching: ", err)
